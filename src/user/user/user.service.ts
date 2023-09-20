3a86d08e7a05ccad758f05c23f1840877b97b936
Elvis DAndrea <elvis.gravi@gmail.com>
2023-09-20 15:15:23 -0300

    mudei tal coisa

--- src/user/user/user.service.ts
+++ src/user/user/user.service.ts
@@ -19,7 +19,7 @@ export class UserService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return `sidyfgsuydgfusdygfugfyds #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
